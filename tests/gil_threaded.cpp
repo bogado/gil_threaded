365277e6e3388aa489f9a2793a26ac533882216a
Victor Bogado <victor@bogado.net>
2009-09-15 10:52:50 -0300

    Unit testing is working.
    2 tests already made:
    	- fill_pixels
    	- for_each_pixel
    	- transform_pixels

--- tests/gil_threaded.cpp
+++ tests/gil_threaded.cpp
@@ -1,5 +1,8 @@
-#define BOOST_TEST_MODULE gil_threaded test
-#include <boost/test/unit_test.hpp>
+#include <iostream>
+#include <algorithm>
+
+#define BOOST_TEST_MODULE MyTest
+#include <boost/test/included/unit_test.hpp>
 
 #include <boost/gil/gil_all.hpp>
 #include <boost/gil/extension/io/jpeg_dynamic_io.hpp>
@@ -7,13 +10,85 @@
 
 namespace gil = boost::gil;
 
-BOOST_AUTO_TEST_CASE(fill_pixels)
+gil::rgb8_image_t load_image()
 {
-	gil::rgb8_image_t file;
-	gil::rgb8_view_t v = gil::view(file);
+	gil::rgb8_image_t image;
 
+	gil::rgb8_view_t v = gil::view(image);
 	gil::fill_pixels(v, gil::rgb8_pixel_t(0,0,0));
-	gil::jpeg_read_view("ramses.jpg", v);
 
-//	BOOST_REQUIRE(std::count(v.begin(), v.end(), gil::rgb8_pixel_t(0,0,0)) != v.size());
+	gil::jpeg_read_image("ramses.jpg", image);
+	v = gil::view(image);
+
+	BOOST_REQUIRE_NE(std::count(v.begin(), v.end(), gil::rgb8_pixel_t(0,0,0)), v.size());
+
+	return image;
+}
+
+BOOST_AUTO_TEST_CASE(fill)
+{
+	gil::rgb8_image_t image = load_image();
+	gil::rgb8_view_t v = gil::view(image);
+
+	gil::threaded::fill_pixels(v, gil::rgb8_pixel_t(0,128,255));
+	
+	BOOST_CHECK_EQUAL(std::count(v.begin(), v.end(), gil::rgb8_pixel_t(0,128,255)), v.size());
+}
+
+struct count_red
+{
+	size_t val;
+
+	count_red() : val(0)
+	{}
+
+	void operator()(gil::rgb8_pixel_t pix)
+	{
+		if (gil::at_c<0>(pix) > 128 && gil::at_c<1>(pix) < 128 && gil::at_c<2>(pix) < 128)
+			val++;
+	}
+};
+
+BOOST_AUTO_TEST_CASE(for_each_pixel)
+{
+	// image to be processed with the usual method.
+	gil::rgb8_image_t image1 = load_image();
+	gil::rgb8_view_t v1 = gil::view(image1);
+	count_red c1;
+
+	// image to be processed with multithreads.
+	gil::rgb8_image_t image2 = load_image();
+	gil::rgb8_view_t v2 = gil::view(image2);
+	count_red c2;
+
+	gil::for_each_pixel          (v1, c1);
+	gil::threaded::for_each_pixel(v2, c2);
+
+	BOOST_WARN_EQUAL (c1.val, 0);
+	BOOST_CHECK_EQUAL(c1.val, c2.val);
+}
+
+gil::rgb8_pixel_t invert_pixel(const gil::rgb8_pixel_t& orig)
+{
+	return gil::rgb8_pixel_t(
+			gil::at_c<0>(orig) ^ 0xff,
+			gil::at_c<1>(orig) ^ 0xff,
+			gil::at_c<2>(orig) ^ 0xff);
+}
+
+BOOST_AUTO_TEST_CASE(transform_pixels)
+{
+	gil::rgb8_image_t image1 = load_image();
+	gil::rgb8_view_t orig    = gil::view(image1);
+
+	gil::rgb8_image_t dest1(orig.dimensions());
+	gil::rgb8_image_t dest2(orig.dimensions());
+
+	gil::rgb8_view_t v1 = gil::view(dest1);
+	gil::rgb8_view_t v2 = gil::view(dest2);
+
+	gil::transform_pixels          (orig, v1, &invert_pixel);
+	gil::threaded::transform_pixels(orig, v2, &invert_pixel);
+
+	BOOST_CHECK(std::equal(v1.begin(), v1.end(), v2.begin()));
 }
